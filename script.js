d76ba34dbd01c7570e10dd27da06869d32716b56
KevinAlvss <kevin.bernardo64@gmail.com>
2023-08-12 12:27:01 -0300

    atualizando pra uma atividade da faculdade

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const titulo = document.querySelector(".maquina-escrever");
 
 function typeWrite(elemento) {
-    const textoArray = elemento.innerHTML.split("");
-    elemento.innerHTML = " ";
-    textoArray.forEach(function (letra, i) {
-        setTimeout(function () {
-        elemento.innerHTML += letra;
-        }, 100 * i);
-    });
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = " ";
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra;
+    }, 150 * i);
+  });
 }
 typeWrite(titulo);
 
 const sr = ScrollReveal({
-    origin: "top",
-    distance: "70px",
-    duration: 2000,
-  });
-  
+  origin: "top",
+  distance: "70px",
+  duration: 2000,
+});
+
 sr.reveal(".sobre-mim-text-box", { delay: 400 });
 sr.reveal(".sobre-mim-img", { delay: 200 });
 sr.reveal(".sr-apresentacao", { delay: 300 });
